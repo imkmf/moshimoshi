c50f7039769f7b2500d6172c96c034d8cedd142b
Kristian Freeman <kristian@kristianfreeman.com>
2013-05-21 16:50:01 -0700

    Change error messages

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Ohayo::Application.routes.draw do
   resources :users, except: :index
   root to: "users#index", constraints: lambda { |r| r.env["warden"].authenticate? }
   root to: 'high_voltage/pages#show', :id => 'home'
+  match "/404", :to => "high_voltage/pages#show", :id => "404"
+  match "/422", :to => "high_voltage/pages#show", :id => "422"
+  match "/500", :to => "high_voltage/pages#show", :id => "500"
 end
